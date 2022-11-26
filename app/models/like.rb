fcdbbcd5a168df38aefe1cca38c45744fb7d4239
Lena <summyalena@gmail.com>
2022-11-26 23:40:48 +0100

    linter erros

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,8 @@
 class Like < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :post
-  
-    def update_likes_counter
-      post.increment!(:likes_count)
-    end
+  belongs_to :user
+  belongs_to :post
+
+  def update_likes_counter
+    post.increment!(:likes_count)
   end
-  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
-    has_one :posts
-    has_many :comments, through: :posts
-    has_many :likes, through: :posts
-  
-    def recent_three_posts
-      posts.last(3)
-    end
+  has_one :posts
+  has_many :comments, through: :posts
+  has_many :likes, through: :posts
+
+  def recent_three_posts
+    posts.last(3)
   end
-  
+end
