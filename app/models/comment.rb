7159d34591b7431e42eb09e3457c84b318fcf0b9
Lena <summyalena@gmail.com>
2022-11-26 23:26:07 +0100

    model for process data

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :post
+  
+    def update_comments_counter
+      post.increment!(:comments_count)
+    end
+  end
+  
---
+++ app/models/like.rb
@@ -0,0 +1,9 @@
+class Like < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :post
+  
+    def update_likes_counter
+      post.increment!(:likes_count)
+    end
+  end
+  
