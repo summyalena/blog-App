06b1b4352fe70af54bd593b84d1a070f55b310c4
Lena <summyalena@gmail.com>
2022-11-28 08:45:52 +0100

    fixed linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-    has_one :posts
-    has_many :comments, through: :posts
-    has_many :likes, through: :posts
-    validates :name, presence: true
-    validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
-    def recent_three_posts
-      posts.last(3)
-    end
+  has_one :posts
+  has_many :comments, through: :posts
+  has_many :likes, through: :posts
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def recent_three_posts
+    posts.last(3)
   end
+end
