958292dd07aabe36ef7f1ca22a3a9a037c1782cc
Lena <summyalena@gmail.com>
2022-12-02 09:46:28 +0100

    update

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,6 @@
 class Like < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :author, :foreign_key => :author_id, :primary_key => :uid
+  class_name: 'User'
   belongs_to :post
 
   def update_likes_counter
