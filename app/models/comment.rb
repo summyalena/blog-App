9d0e6d0b396766840920ff585b87451d36c9acbc
Lena <summyalena@gmail.com>
2022-12-02 10:02:53 +0100

    making updates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :author, foreign_key: :author_id, primary_key: :uid
+  class_name :User
   belongs_to :post
 
   def update_comments_counter
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,9 @@
 class Like < ActiveRecord::Base
-    belongs_to :author, :foreign_key => :author_id, :primary_key => :uid
-    class_name: 'User'
-    belongs_to :post
-  
-    def update_likes_counter
-      post.increment!(:likes_count)
-    end
+  belongs_to :author, foreign_key: :author_id, primary_key: :uid
+  class_name :User
+  belongs_to :post
+
+  def update_likes_counter
+    post.increment!(:likes_count)
   end
+end
