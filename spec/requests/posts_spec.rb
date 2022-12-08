d5978fd88dfda890df876da566f5b0095aaf6e65
Lena <summyalena@gmail.com>
2022-12-08 13:18:57 +0100

    linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe 'Posts', type: :request do
   before(:each) do
     @user = User.create(name: 'Alena', posts_counter: 2)
     @post = Post.create(
@@ -15,37 +15,36 @@ RSpec.describe "Posts", type: :request do
     Comment.create(text: 'mamah love you')
   end
 
-  describe "GET /index" do
+  describe 'GET /index' do
     before(:each) do
       get user_posts_path(@user)
     end
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(200)
     end
-    it "return rendered template" do
+    it 'return rendered template' do
       expect(response).to render_template(:index)
     end
-    it "response body includes placeholer" do
-      expect(response.body).to include "Find me in app/views/posts/index.html.erb"
+    it 'response body includes placeholer' do
+      expect(response.body).to include 'Find me in app/views/posts/index.html.erb'
     end
   end
 
-  describe "GET /show" do
+  describe 'GET /show' do
     before(:each) do
       get user_posts_path(@user, @post)
     end
 
-    it "returns http success" do
-      get "/posts/show"
+    it 'returns http success' do
+      get '/posts/show'
       expect(response).to have_http_status(200)
     end
-    it "rendered template" do
-      get "/posts/show"
+    it 'rendered template' do
+      get '/posts/show'
       expect(response).to render_template(:show)
     end
-    it "check for placeholder text" do
-      expect(response.body).to include "Find me in app/views/posts/show.html.erb"
+    it 'check for placeholder text' do
+      expect(response.body).to include 'Find me in app/views/posts/show.html.erb'
     end
   end
-
 end
