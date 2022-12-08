02c7b8b65fa4df57ee4ecef1a9e6ccae01bf0da6
Lena <summyalena@gmail.com>
2022-12-08 13:07:08 +0100

    linters correction

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   before(:each) do
--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative 'rails_helper'
 
 RSpec.describe 'posts/index.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
