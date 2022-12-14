d12c6cbdcb5231000e0903c9c4cc49d2a34f1819
Lena <summyalena@gmail.com>
2022-12-14 09:53:27 +0100

    applcation-controller error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     protect_from_forgery with: :null_session
     before_action :configure_permitted_parameters, if: :devise_controller?
-  
+    user_signed_in?
+    user_session
+    
     protected
-  
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
