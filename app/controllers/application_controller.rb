f3d176f18a4588c3d0bf6159864c85dbae75f995
Lena <summyalena@gmail.com>
2022-12-16 15:00:14 +0100

    authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     protect_from_forgery with: :null_session
     before_action :configure_permitted_parameters, if: :devise_controller?
-    user_signed_in?
-    user_session
-    
     protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+        devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+          user_params.permit({ roles: [] }, :email, :password, :password_confirmation)
+        end
+      end
+      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,1 @@
-Rails.application.routes.draw do
-  devise_for :users
-  get 'likes/new'
-  get 'comments/new'
-  root 'users#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :create, :new] do
-      resources :likes, only: [:create] 
-      resources :comments, only:[:create, :new]
-    end
-  end
-end
+
